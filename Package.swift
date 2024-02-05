cbdd989a5f8f29819e4fc433c0c2f32af484e800
nk-5 <keigo.nakagawa@luxgo.jp>
2024-02-05 12:28:30 +0900

    support spm

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "unity-ads-ios",
+    products: [
+        .library(
+            name: "unity-ads-ios",
+            targets: ["unity-ads-ios"]),
+    ],
+    targets: [
+        // .target(
+        //     name: "unity-ads-ios",
+        //     path: "SourceCode"
+        // ),
+        .binaryTarget(
+            name: "unity-ads-ios",
+            url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.8.0/UnityAds.zip", 
+            checksum: "8fd3a218ca955868f26abeb9da889dbc6c1ff90ffae076bdccb715f2a1fd6448"
+        ),
+    ]
+)
